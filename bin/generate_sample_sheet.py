e34ffa142d577f720065f4d90061f5a5c20116fe
Ernst Hamer <ernst.hamer@casema.nl>
2020-06-22 09:45:19 +0200

    update samplesheet.py
    revert jovian changes because they don't work

--- bin/generate_sample_sheet.py
+++ bin/generate_sample_sheet.py
@@ -22,7 +22,7 @@ import re
 import yaml
 
 
-fq_pattern = re.compile("(.*)(_|\.)R?(1|2)(?:_.*\.|\..*\.|\.)f(ast)?q(\.gz)?")
+fq_pattern = re.compile("(.*)_R?(1|2)(?:_.*\.|\..*\.|\.)f(ast)?q(\.gz)?")
 
 def main(args):
     assert args.dir.is_dir(), "Argument must be a directory."
@@ -32,9 +32,8 @@ def main(args):
         if file_.is_dir():
             continue
         match = fq_pattern.fullmatch(file_.name)
-        if match:
-          sample = samples.setdefault(match.group(1), {})
-          sample["R{}".format(match.group(3))] = str(file_)
+        sample = samples.setdefault(match.group(1), {})
+        sample["R{}".format(match.group(2))] = str(file_)
 
     print(yaml.dump(samples, default_flow_style=False))
 
