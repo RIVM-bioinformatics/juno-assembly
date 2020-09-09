658658b267cfd8fb7949502fff4a2d429b8ec52a
hernanda <alejandra.hernandez.segura@rivm.nl>
2020-09-09 16:21:20 +0200

    Fixed bug with samples with a . (dot) on the file name

--- bin/fastqc_wrapper.sh
+++ bin/fastqc_wrapper.sh
@@ -16,8 +16,9 @@ DESIRED_OUTPUT_ZIP="$4"
 LOG="$5"
 
 # Generate sample basename (i.e. remove everything before the last '/' and then remove everything after the '.')
-SAMPLE_TEMP="${INPUT_FILE##*/}"
-SAMPLE_NAME="${SAMPLE_TEMP%%.*}"
+SAMPLE_TEMP=`basename $INPUT_FILE`
+#"${INPUT_FILE##*/}"
+SAMPLE_NAME="${SAMPLE_TEMP%.f*q*}"
 
 # These are the output files that fastqc implicitly generates (based on the input file basename)
 REAL_OUTPUT_HTML="${OUTPUT_DIR}/${SAMPLE_NAME}_fastqc.html"
