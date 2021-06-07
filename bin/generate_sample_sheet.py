d21cb910b06e0ed415057a0c48ed6d7a723d5cce
AleSR13 <ale.hdz.segura@gmail.com>
2021-06-07 16:50:41 +0200

    Adding threads to SPAdes command and checking file size before running pipeline

--- bin/generate_sample_sheet.py
+++ bin/generate_sample_sheet.py
@@ -20,6 +20,7 @@ import argparse
 import pathlib
 import re
 import yaml
+import warnings
 
 fq_pattern = re.compile("(.*?)(?:_S\d+_|_S\d+.|_|\.)R?(1|2)(?:_.*\.|\..*\.|\.)f(ast)?q(\.gz)?")
 
@@ -27,14 +28,20 @@ def main(args):
     assert args.dir.is_dir(), "Argument must be a directory."
     
     samples = {}
+    small_files = []
     for file_ in args.dir.iterdir():
         if file_.is_dir():
             continue
-        match = fq_pattern.fullmatch(file_.name)
-        if match:
-            sample = samples.setdefault(match.group(1), {})
-            sample["R{}".format(match.group(2))] = str(file_)
+        if file_.stat().st_size > 3000:
+            match = fq_pattern.fullmatch(file_.name)
+            if match:
+                sample = samples.setdefault(match.group(1), {})
+                sample["R{}".format(match.group(2))] = str(file_)
+        else:
+            small_files.append(str(file_))
 
+    if len(small_files) > 0:   
+        warnings.warn("\n\n\033[91mThe following files are too small (<3000 bytes) and were not included in the analysis: \n-{}\033[0m\n\n".format('\n-'.join(small_files)))
     print(yaml.dump(samples, default_flow_style=False))
 
 
